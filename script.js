51650885cf17c5af064dbe55da7d66a84dc483fc
salsabil <salsabilalimahmoud@gmail.com>
2025-09-03 20:57:14 +0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-function showPlan() {
+ function showPlan() {
             document.getElementById('welcome').style.display = 'none';
             document.getElementById('plan').style.display = 'block';
+               document.getElementById('back-arrow').scrollIntoView({behavior: 'smooth', block: 'start'});
         }
         function showWelcome() {
-            document.getElementById('plan').style.display = 'none';
-            document.getElementById('welcome').style.display = 'flex';
-        }
+    document.getElementById('plan').style.display = 'none';
+    document.getElementById('welcome').style.display = 'flex';
+}
